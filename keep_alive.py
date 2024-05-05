3e56c5dd76915ca418572acd0764c250377393ca
me1231232dd <102393010+me1231232dd@users.noreply.github.com>
2024-05-05 12:57:35 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -4,8 +4,6 @@ from threading import Thread
 app = Flask('')
 
 @app.route('/')
-def main():
-    return '<meta http-equiv="refresh" content="0; URL=https://phantom.is-a.dev/support"/>'
 
 def run():
     app.run(host="0.0.0.0", port=8080)
