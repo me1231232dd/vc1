ef444e945b4a10ea58bf51f76e4887429dee65ed
me1231232dd <102393010+me1231232dd@users.noreply.github.com>
2024-05-05 13:07:14 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,9 +1,16 @@
-from flask import Flask
+try:
+	from flask import Flask
+except:
+	from setup import install
+	install()
+	from flask import Flask
 from threading import Thread
 
 app = Flask('')
 
 @app.route('/')
+def main():
+    return "Voicecord online!"
 
 def run():
     app.run(host="0.0.0.0", port=8080)
